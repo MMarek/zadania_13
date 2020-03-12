356356cf87558fc6d89eec97379b391a001fb886
MMarek <mildnermarek@gmail.com>
2020-03-12 13:54:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {
+    HashRouter,
+    Route,
+} from 'react-router-dom';
+
+import Home from "./components/home"
+
+function App() {
+    return (
+        <HashRouter>
+        <Route exact path='/' component={Home}/>
+    </HashRouter>
+);
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Task01 from "./task01";
+
+class Home extends Component {
+
+    render() {
+        return (
+            <>
+                <Task01/>
+            </>
+        );
+    }
+}
+
+export default Home;
